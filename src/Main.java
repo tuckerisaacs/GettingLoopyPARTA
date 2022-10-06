9ea93c8c456c79c303f06155070093abcd7523bf
tucker isaacs <isaacstb@mail.uc.edu>
2022-10-06 14:28:08 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main
+{
+    public static void main(String[] args)
+
+    {
+
+       for(int a = 1; a <= 30; a++)
+       {
+           System.out.println(a);
+       }
+        for(int b = 30; b >= 0; b--)
+        {
+            System.out.println(b);
+        }
+        for(int c = 0; c <= 18; c += 3)
+        {
+            System.out.println(c);
+        }
+        for(int d = 10; d >= 0; d -= 2)
+        {
+            System.out.println(d);
+        }
+        
+
+
+
+
+
+
+
+
+    }
+
+}
